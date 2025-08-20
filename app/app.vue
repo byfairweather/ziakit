d029b67e70d6f8aa03858831cfe6014ebf9788dd
Daniel Fairweather <byfairweather@gmail.com>
2025-08-20 16:21:41 -0600

    Setup Nuxt

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Welcome to Ziakit!</div>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: "Ziakit",
+    meta: [{ name: "description", content: "Nuxt UI Toolkit" }],
+});
+</script>
